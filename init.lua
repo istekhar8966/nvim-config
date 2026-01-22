e97b823752bf272b39b35645165102ffee8c601d
Md istekhar <istekhar8966@gmail.com>
2026-01-22 13:32:35 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,243 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- ==========================================
+-- 1. CORE SETTINGS
+-- ==========================================
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.mouse = a
 
--- Load core configuration
-require('core.options')
-require('core.keymaps')
-require('core.autocmds')
+vim.opt.updatetime = 100
+vim.opt.timeoutlen = 300
+vim.opt.lazyredraw = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
 
--- Load plugin configuration
-require('config.lazy')
+-- ==========================================
+-- 2. BOOTSTRAP lazy.nvim
+-- ==========================================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ==========================================
+-- 3. PLUGINS (Cutting-Edge)
+-- ==========================================
+require("lazy").setup({
+  -- Colorscheme
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+        italic = { strings = false, comments = false },
+      })
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+
+  -- Fuzzy Finder (Lua/C)
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<C-p>", "<cmd>FzfLua files<cr>" },
+      { "<leader>fg", "<cmd>FzfLua live_grep<cr>" },
+      { "<leader>fb", "<cmd>FzfLua buffers<cr>" },
+    },
+    opts = {
+      winopts = { height = 0.85, width = 0.80 },
+    },
+  },
+
+  -- Mason (LSP installer)
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    build = ":MasonUpdate",
+    opts = { ui = { border = "single" } },
+  },
+
+  -- Blink.cmp (Completion in C - FAST)
+  {
+    "saghen/blink.cmp",
+    lazy = false,
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "path", "buffer" },
+      },
+      completion = {
+        menu = {
+          border = "single",
+          draw = {
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+      },
+    },
+    opts_extend = { "sources.default" },
+  },
+}, {
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin",
+      },
+    },
+  },
+})
+
+-- ==========================================
+-- 4. NATIVE LSP (Neovim 0.11+)
+-- ==========================================
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { buffer = args.buf, silent = true }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  end,
+})
+
+-- Rust
+vim.lsp.config('rust_analyzer', {
+  cmd = { 'rust-analyzer' },
+  root_markers = { 'Cargo.toml' },
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = { allFeatures = true },
+      checkOnSave = { command = 'clippy' },
+    }
+  },
+})
+
+-- C/C++
+vim.lsp.config('clangd', {
+  cmd = { 'clangd', '--background-index', '--clang-tidy' },
+  root_markers = { 'compile_commands.json', '.git' },
+})
+
+-- Auto-enable
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "rust", "c", "cpp" },
+  callback = function()
+    if vim.fn.expand("%:p") ~= "" then
+      local ft = vim.bo.filetype
+      if ft == "rust" then
+        vim.lsp.enable('rust_analyzer')
+      elseif ft == "c" or ft == "cpp" then
+        vim.lsp.enable('clangd')
+      end
+    end
+  end,
+})
+
+-- ==========================================
+-- 5. NETRW
+-- ==========================================
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 25
+vim.keymap.set("n", "<leader>e", ":Lex<CR>", { silent = true })
+
+-- ==========================================
+-- 6. TERMINAL (Ctrl + /)
+-- ==========================================
+local term_buf = nil
+local term_win = nil
+
+local function toggle_terminal()
+  if term_win and vim.api.nvim_win_is_valid(term_win) then
+    vim.api.nvim_win_hide(term_win)
+    term_win = nil
+  else
+    vim.cmd("botright 15split")
+    if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+      vim.api.nvim_win_set_buf(0, term_buf)
+    else
+      vim.cmd("terminal")
+      term_buf = vim.api.nvim_get_current_buf()
+    end
+    term_win = vim.api.nvim_get_current_win()
+    vim.cmd("startinsert")
+  end
+end
+
+-- ==========================================
+-- 8. CLIPBOARD (Separate System vs Internal)
+-- ==========================================
+-- System clipboard (leader y/p)
+vim.keymap.set({"n", "v"}, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set({"n", "v"}, "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+vim.keymap.set({"n", "v"}, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({"n", "v"}, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+
+-- Internal clipboard (default y/p) - no remapping needed, works by default
+-- This keeps 'y' and 'p' working only within Neovim
+
+vim.keymap.set("n", "<C-_>", toggle_terminal, { silent = true })
+vim.keymap.set("t", "<C-_>", toggle_terminal, { silent = true })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
+
+
+-- ==========================================
+-- UNDO HISTORY (Persistent + Visualization)
+-- ==========================================
+-- Enable persistent undo (survives nvim restarts)
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+vim.opt.undolevels = 10000
+vim.opt.undoreload = 10000
+
+-- Create undo directory if it doesn't exist
+vim.fn.mkdir(vim.fn.stdpath("data") .. "/undo", "p")
+
+-- Keybinds
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo Tree" })
+
+
+-- ==========================================
+-- 7. PERFORMANCE
+-- ==========================================
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave" }, {
+  callback = function() vim.opt.relativenumber = true end
+})
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter" }, {
+  callback = function() vim.opt.relativenumber = false end
+})
